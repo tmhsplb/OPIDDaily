ce7776449d97434e5d3c302d43154116a0eda9fb
Peter Bruell <peter3418@ymail.com>
2019-07-19 19:08:55 -0500

    Moved entities to a OPIDaily folder.

---
+++ OPIDDaily/Entities/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OpidDailyEntities
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime ServiceDate { get; set; }
+
+        public string ServiceTicket { get; set; }
+
+        public int WaitTime { get; set; }
+
+        public string Stage { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string BirthName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public int Age { get; set; }
+        
+        public string Notes { get; set; }
+
+        public DateTime Screened { get; set; }
+
+        public DateTime CheckedIn { get; set; }
+
+        public DateTime Interviewing { get; set; }
+
+        public DateTime Interviewed { get; set; }
+
+        public DateTime BackOffice { get; set; }
+
+        public DateTime Done { get; set; }
+
+        public bool PND { get; set; }
+
+        public bool XID { get; set; }
+
+        public bool XBC { get; set; }
+
+        public bool Active { get; set; }
+
+        public ICollection<Visit> Visits { get; set; }
+    }
+}
---
+++ OPIDDaily/Entities/Visit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OpidDailyEntities
+{
+    public class Visit
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Item { get; set; }
+
+        public string Check { get; set; }
+
+        public string Status { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
