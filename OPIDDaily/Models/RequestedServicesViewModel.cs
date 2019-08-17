9952449f215c41dbfa8afbb29bf086f2d8f79765
Peter Bruell <peter3418@ymail.com>
2019-08-17 14:47:05 -0500

    Added Supporting Documents for Interviewer role.

--- OPIDDaily/Models/RequestedServicesViewModel.cs
+++ OPIDDaily/Models/RequestedServicesViewModel.cs
@@ -24,7 +24,7 @@ namespace OPIDDaily.Models
         public bool BCEligible { get; set; }
 
         [Display(Name = "HCC")]
-        public bool HCC { get; set;}
+        public bool HCC { get; set; }
 
         [Display(Name = "MBVD")]
         public bool MBVD { get; set; }
@@ -37,12 +37,10 @@ namespace OPIDDaily.Models
         [Display(Name = "New/Dup ID")]
         public bool NewTID { get; set; }
 
-      
-
         [Display(Name = "Eligible?")]
         public bool NewTIDEligible { get; set; }
 
-        [Display(Name = "Replcmnt ID")]
+        [Display(Name = "Replacement ID")]
         public bool ReplacementTID { get; set; }
 
         [Display(Name = "Eligible?")]
@@ -56,10 +54,59 @@ namespace OPIDDaily.Models
         [Display(Name = "Eligible?")]
         public bool ReplacementTDLEligible { get; set; }
 
-        [Display(Name = "Replcmnt DL")]
+        [Display(Name = "Replacement DL")]
         public bool ReplacementTDL { get; set; }
 
         [Display(Name = "Numident")]
         public bool Numident { get; set; }
+
+        [Display(Name = "Birth Certificate")]
+        public bool SDBC { get; set; }
+
+        [Display(Name = "SS Card")]
+        public bool SDSSC { get; set; }
+
+        [Display(Name = "Texas ID")]
+        public bool SDTID { get; set; }
+
+        [Display(Name = "Texas DL")]
+        public bool SDTDL { get; set; }
+
+        [Display(Name = "Other State ID")]
+        public bool SDOSID { get; set; }
+
+        [Display(Name = "Other State DL")]
+        public bool SDOSDL { get; set; }
+
+        [Display(Name = "Marriage License")]
+        public bool SDML { get; set; }
+
+        [Display(Name = "Divorce Decree")]
+        public bool SDDD { get; set; }
+
+        [Display(Name = "Service Letter")]
+        public bool SDSL { get; set; }
+
+        [Display(Name = "DD214")]
+        public bool SDDD214 { get; set; }
+
+        [Display(Name = "Gold Card")]
+        public bool SDGC { get; set; }
+
+        [Display(Name = "EBT Card")]
+        public bool SDEBT { get; set; }
+
+        [Display(Name = "HOTID")]
+        public bool SDHOTID { get; set; }
+
+        [Display(Name = "School Records")]
+        public bool SDSchoolRecords { get; set; }
+
+        [Display(Name = "Passport")]
+        public bool SDPassport { get; set; }
+
+        [Display(Name = "Job Offer Letter")]
+        public bool SDJobOffer { get; set; }
+
     }
 }
