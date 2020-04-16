8a0cf46562a3b851f97ca77defec772a58fcd02d
Peter Bruell <peter3418@ymail.com>
2020-04-16 14:57:00 -0500

    Added How it Works

---
+++ OPIDDaily/Scripts/HubCode/CaseManagerHubCode.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    var theHub = $.connection.dailyHub;
+
+    theHub.client.refreshPage = function () {
+        var currentPage = jQuery("#clientsGrid").jqGrid('getGridParam', 'page');
+        // alert("currentPage = " + currentPage)
+        var url = "GetMyClients?page=pageToken&rows=25",
+        // @Url.Action("GetMyClients", "CaseManager", new { page = "pageToken", rows = "rowsToken" }) ";
+
+        url = url.replace("pageToken", currentPage);
+        url = url.replace("rowsToken", 25);
+        // alert("url = " + url);
+
+        $.ajax({
+            url: url,
+            cache: false,
+            dataType: "json",
+            success: function (data) {
+                var mygrid = jQuery("#clientsGrid")[0];
+                mygrid.addJSONData(data);
+            }
+        });
+    };
+
+    $.connection.hub.start().done(function () {
+    });
+});
